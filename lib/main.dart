c1f3d6bc5b4a43a51a036df5de165d5fe8e0edf1
unknown <otienomichael360@gmail.com>
2024-04-24 15:59:02 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'Pages/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primaryColor: Colors.green,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
